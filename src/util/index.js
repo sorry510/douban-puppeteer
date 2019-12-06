3005e7e621712015d68b3de6d6e173b7b03fb38d
sorry510 <sorry510sf@gmail.com>
2019-12-06 18:42:52 +0800

    fix: go home

--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,21 @@
+// 匹配小括号中的内容
 const regIncludes = (str)=> {
 	const res = str.match(/\((.+?)\)/g)
 	return res ? JSON.stringify(res.map(item=> item.match(/\((.+?)\)/)[1])) : '[]'
 }
 
+// 是否包含中文
+const includeZh = (str)=> {
+	return /.*[\u4e00-\u9fa5]+.*$/.test(str)
+}
+
+// 分隔字符串返回json结构
+const jsonSplitStr = (str)=> {
+	return JSON.stringify(str.trim().split('/').map(v=> v.trim()))
+}
+
 module.exports = {
-  regIncludes
+	regIncludes,
+	includeZh,
+	jsonSplitStr
 }
