230ec80e4cc6362a957b2abcf9428147568ca471
sorry510 <sorry510sf@gmail.com>
2019-12-12 11:45:04 +0800

    fix: 修改数据库

--- sql/t_douban_player.sql
+++ sql/t_douban_player.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2019-12-12 11:04:54
+Date: 2019-12-12 11:43:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,19 +22,19 @@ DROP TABLE IF EXISTS `t_douban_player`;
 CREATE TABLE `t_douban_player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `playerId` varchar(10) DEFAULT NULL COMMENT '演员/导演id',
-  `name` varchar(20) DEFAULT '' COMMENT '演员名称',
+  `name` varchar(300) DEFAULT '' COMMENT '演员名称',
   `avatar` varchar(300) DEFAULT '' COMMENT '头像',
   `sex` char(1) DEFAULT '',
   `constellation` varchar(100) DEFAULT '' COMMENT '星座',
   `birthday` varchar(50) DEFAULT '' COMMENT '出生日期',
   `birthplace` varchar(100) DEFAULT '' COMMENT '出生地',
   `role` varchar(255) DEFAULT '' COMMENT '职业',
-  `name_en` varchar(50) DEFAULT '' COMMENT '更多外文名',
-  `name_cn` varchar(50) DEFAULT '' COMMENT '更多中文名',
+  `name_en` varchar(300) DEFAULT '' COMMENT '更多外文名',
+  `name_cn` varchar(300) DEFAULT '' COMMENT '更多中文名',
   `family` varchar(100) DEFAULT '' COMMENT '家庭成员',
   `introduce` text DEFAULT NULL COMMENT '人物简介',
   `movies` text DEFAULT NULL COMMENT '作品信息json字符串',
   `update_time` bigint(20) DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `playerId` (`playerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=12298 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12304 DEFAULT CHARSET=utf8;
